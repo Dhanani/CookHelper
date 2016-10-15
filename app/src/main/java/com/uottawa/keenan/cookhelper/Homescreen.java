908b6c63c3f2cfc279307d38c1c25b15b78a4b19
Keenan Dhanani <keenandhanani@gmail.com>
2016-10-15 16:02:00 -0400

    progess on 'add recipe' view

--- app/src/main/java/com/uottawa/keenan/cookhelper/Homescreen.java
+++ app/src/main/java/com/uottawa/keenan/cookhelper/Homescreen.java
@@ -1,7 +1,9 @@
 package com.uottawa.keenan.cookhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Homescreen extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Homescreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+    }
+
+    public void OnAddRecipe(View view) {
+        Intent intent = new Intent(getApplicationContext(), AddRecipe.class);
+        startActivityForResult (intent,0);
+
     }
 }
