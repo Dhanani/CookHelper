96e621444f8b4b7c69bdb7cd53b3bbe5a85e3971
Karim Chukfeh <Karim Chukfeh>
2016-10-18 18:26:03 -0400

    added the Help activity and important String values

--- app/src/main/java/com/uottawa/keenan/cookhelper/Homescreen.java
+++ app/src/main/java/com/uottawa/keenan/cookhelper/Homescreen.java
@@ -17,4 +17,9 @@ public class Homescreen extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), AddRecipe.class);
         startActivityForResult (intent,0);
     }
+
+    public void OnHelp(View view){
+        Intent intent = new Intent(getApplicationContext(), Help.class);
+        startActivity(intent);
+    }
 }
