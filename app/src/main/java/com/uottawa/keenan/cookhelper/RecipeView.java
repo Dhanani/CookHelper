79c2358317963af370fd558e093052329127ff89
Hasnain Syed <hasnain985@gmail.com>
2016-11-29 23:19:17 -0500

    Added RecipeView activity

---
+++ app/src/main/java/com/uottawa/keenan/cookhelper/RecipeView.java
@@ -0,0 +1,13 @@
+package com.uottawa.keenan.cookhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_view);
+    }
+}
